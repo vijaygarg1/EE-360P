c11730ebe75441f6717caf530499b704d78e04ea
Wei-Lun Hung <kinmener@gmail.com>
2016-01-13 12:06:33 -0600

    add test cases and templates for hw1

---
+++ homework-template/hw1/q1/PSort.java
@@ -0,0 +1,5 @@
+public abstract class PSort{
+  public static void parallelSort(int[] A, int begin, int end){
+    // TODO: Implement your parallel sort function 
+  }
+}
---
+++ homework-template/hw1/q2/PSearch.java
@@ -0,0 +1,6 @@
+public abstract class PSearch{
+  public static int parallelSearch(int k, int[] A, int numThreads){
+    // TODO: Implement your parallel search function 
+    return -1;
+  }
+}
