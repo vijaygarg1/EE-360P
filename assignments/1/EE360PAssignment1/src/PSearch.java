7d0ec02aac627c07815d61de2fcbba81058ac5d5
William Mauldin <stormymauldin@utexas.edu>
2016-01-25 10:56:34 -0600

    Added Assignment 1 files to create a dir structure

---
+++ assignments/1/EE360PAssignment1/src/PSearch.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Callable;
+
+public class PSearch implements Callable {
+	
+	public static int parallelSearch(int x, int[] A, int numThreads) {
+		return 0;		//Needs to return -1 or index of x
+		
+	}
+	
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ assignments/1/EE360PAssignment1/src/PSort.java
@@ -0,0 +1,13 @@
+public class PSort implements Runnable {
+	
+	public static void parallelSort(int[] A, int begin, int end) {
+		int pivot = begin;
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
