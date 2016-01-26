35d80358e999a9addf285e2703c0a44c7345565f
Eric Crosson <esc@ericcrosson.com>
2016-01-26 09:19:02 -0600

    restructuring project repo, add javadoc comments

---
+++ assignments/pset1/src/PSearch.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.Callable;
+
+/**
+ * @author      Eric Crosson <eric.s.crosson@utexas.edu>
+ * @author      William "Stormy" Mauldin <stormymauldin@utexas.edu>
+ * @version     0.1
+ * @since       2016-01-26
+ */
+public class PSearch implements Callable {
+
+    /**
+     * Search array A for int x with numThreads threads.
+     *
+     * @param  x The search element
+     * @param  A The search space
+     * @param  numThreads The number of searcher threads to spawn
+     * @return The index of x in array A
+     */
+    public static int parallelSearch(int x, int[] A, int numThreads) {
+        return 0;		//Needs to return -1 or index of x
+		
+    }
+	
+    @Override
+    public Object call() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ assignments/pset1/src/PSort.java
@@ -0,0 +1,23 @@
+/**
+ * @author      Eric Crosson <eric.s.crosson@utexas.edu>
+ * @author      William "Stormy" Mauldin <stormymauldin@utexas.edu>
+ * @version     0.1
+ * @since       2016-01-26
+ */
+
+public class PSort implements Runnable {
+	
+    public static void parallelSort(int[] A, int begin, int end) {
+        int pivot = begin;
+		
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+		
+    }
+
+    public static void main(String[] args) {
+    }
+}
