53e4d667ce420163b39c728f32eaa30452f407be
Wei-Lun Hung <kinmener@gmail.com>
2016-02-16 14:21:31 -0600

    add update hw3

--- homework-template/hw3/q3/Server.java
+++ homework-template/hw3/q3/Server.java
@@ -3,13 +3,11 @@ public class Server {
     int N;
     int tcpPort;
     int udpPort;
-    if (args.length != 4) {
-      System.out.println("ERROR: Provide 4 arguments");
-      System.out.println("\t(1) <N>: the total number of available seats");
-      System.out.println("\t\t\tassume the seat numbers are from 1 to N");
-      System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(3) <udpPort>: the port number for UDP connection");
-      System.out.println("\t(4) <file>: the file of inventory");
+    if (args.length != 3) {
+      System.out.println("ERROR: Provide 3 arguments");
+      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
+      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
+      System.out.println("\t(3) <file>: the file of inventory");
 
       System.exit(-1);
     }
