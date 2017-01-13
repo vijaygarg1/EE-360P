1d9f2a978395349b6d6ffcad8b97c578fc378f9d
vijaygarg1 <vkgarg@gmail.com>
2017-01-13 15:51:40 -0600

    he

--- homework-template/hw1/q1/PSort.java
+++
@@ -1,5 +0,0 @@
-public class PSort{
-  public static void parallelSort(int[] A, int begin, int end){
-    // TODO: Implement your parallel sort function 
-  }
-}
--- homework-template/hw1/q2/PSearch.java
+++
@@ -1,6 +0,0 @@
-public class PSearch{
-  public static int parallelSearch(int k, int[] A, int numThreads){
-    // TODO: Implement your parallel search function 
-    return -1;
-  }
-}
