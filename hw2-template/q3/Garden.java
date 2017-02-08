831141d3c24e92cda8a5306feed9f56010021d85
vijaygarg1 <vkgarg@gmail.com>
2017-02-07 19:51:23 -0600

    changed signature for Q3

--- hw2-template/q3/Garden.java
+++ hw2-template/q3/Garden.java
@@ -1,18 +1,18 @@
 public class Garden {
     
   public Garden() {   }; 
-  public synchronized void startDigging() {   }; 
-  public synchronized void doneDigging() {   }; 
-  public synchronized void startSeeding() {   };
-  public synchronized void doneSeeding() {   }; 
-  public synchronized void startFilling() {   }; 
-  public synchronized void doneFilling() {   }; 
+  public void startDigging() {   }; 
+  public void doneDigging() {   }; 
+  public void startSeeding() {   };
+  public void doneSeeding() {   }; 
+  public void startFilling() {   }; 
+  public void doneFilling() {   }; 
  
     /*
     * The following methods return the total number of holes dug, seeded or 
     * filled by Newton, Benjamin or Mary at the time the methods' are 
     * invoked on the garden class. */
-   public synchronized int totalHolesDugByNewton() {   }; 
-   public synchronized int totalHolesSeededByBenjamin() {   }; 
-   public synchronized int totalHolesFilledByMary() {   }; 
+   public int totalHolesDugByNewton() {   }; 
+   public int totalHolesSeededByBenjamin() {   }; 
+   public int totalHolesFilledByMary() {   }; 
 }
