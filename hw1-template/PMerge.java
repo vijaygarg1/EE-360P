1f93792ab9c36eba70399445d1708d535a864532
vijaygarg1 <vkgarg@gmail.com>
2019-01-24 13:58:18 -0600

    init

---
+++ hw1-template/PMerge.java
@@ -0,0 +1,12 @@
+//UT-EID=
+
+
+import java.util.*;
+import java.util.concurrent.*;
+
+
+public class PMerge{
+  public static void parallelMerge(int[] A, int[] B, int[]C, int numThreads){
+    // TODO: Implement your parallel merge function
+  }
+}
---
+++ hw1-template/PSort.java
@@ -0,0 +1,11 @@
+//UT-EID=
+
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class PSort{
+  public static void parallelSort(int[] A, int begin, int end){
+    // TODO: Implement your parallel sort function 
+  }
+}
